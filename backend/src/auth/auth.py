2b3b48b42c74ea36b83fe732e468ed61f66bbf12
malsaadan <mmalsaadan@gmail.com>
2020-07-25 15:51:26 +0300

    Finish edit requires_auth method

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -144,23 +144,16 @@ def verify_decode_jwt(token):
         'description': 'Unable to find the appropriate key.'
         }, 400)
 
-        
-'''
-@TODO implement @requires_auth(permission) decorator method
-    @INPUTS
-        permission: string permission (i.e. 'post:drink')
 
-    it should use the get_token_auth_header method to get the token
-    it should use the verify_decode_jwt method to decode the jwt
-    it should use the check_permissions method validate claims and check the requested permission
-    return the decorator which passes the decoded payload to the decorated method
-'''
 def requires_auth(permission=''):
     def requires_auth_decorator(f):
         @wraps(f)
         def wrapper(*args, **kwargs):
+            # Get token
             token = get_token_auth_header()
+            # Decode jwt
             payload = verify_decode_jwt(token)
+            # Validate claims
             check_permissions(permission, payload)
             return f(payload, *args, **kwargs)
 
